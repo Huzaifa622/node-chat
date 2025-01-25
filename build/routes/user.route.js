17671e113c8ea089367f2c31b1895593b4b64ed8
HuzaifaHNH <thuzaifa07@gmail.com>
2025-01-25 19:46:08 +0500

    ui

--- build/routes/user.route.js
+++ build/routes/user.route.js
@@ -4,6 +4,6 @@ import { auth } from '../middlewares/auth.js';
 const userRouter = Router();
 userRouter.post('/user', createUser);
 userRouter.post('/login', userLogin);
-userRouter.post('/get-all-users', auth, userLogin);
+userRouter.get('/get-all-users', auth, userLogin);
 userRouter.post('/logout', auth, logout);
 export default userRouter;
